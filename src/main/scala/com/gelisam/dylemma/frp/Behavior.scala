038fcb6d793c17f899207d3d73af6c8c50966318
Samuel Gélineau <gelisam+github@gmail.com>
2015-08-10 09:14:07 -0400

    frp version
    looks quite buggy though.

---
+++ src/main/scala/com/gelisam/dylemma/frp/Behavior.scala
@@ -0,0 +1,40 @@
+package com.gelisam.dylemma.frp
+
+import io.dylemma.frp._
+
+case class Behavior[A](var value: A, changeE: EventStream[A])(implicit obs: Observer) {
+  {
+    var nextValue = value
+    changeE.foreach(nextValue = _)
+    Behavior.updateBehaviorsE.foreach(_ => value = nextValue)
+  }
+  
+  def map[B](f: A => B): Behavior[B] =
+    Behavior(f(value), changeE.map(f))
+  def map2[B,C](that: Behavior[B])(f: (A,B) => C): Behavior[C] =
+    Behavior(f(value, that.value), changeE.either(that.changeE).map {
+      case Left(a) => f(a, that.value)
+      case Right(b) => f(value, b)
+    })
+}
+
+object Behavior {
+  implicit def getValue[A]: Behavior[A] => A = _.value
+  
+  val updateBehaviorsE = EventSource[Unit]
+  def updateBehaviors = updateBehaviorsE fire Unit
+  
+  def unit[A](value: A)(implicit obs: Observer): Behavior[A] = {
+    val neverE = EventSource[A]
+    Behavior(value, neverE)
+  }
+  def stepper[A](value: A, changeE: EventStream[A])(implicit obs: Observer): Behavior[A] =
+    Behavior(value, changeE)
+  def accum[A](value: A, changeE: EventStream[A => A])(implicit obs: Observer): Behavior[A] = {
+    var acc = value
+    Behavior(value, changeE.map {f =>
+      acc = f(acc)
+      acc
+    })
+  }
+}
--- src/main/scala/com/gelisam/scage/base/BouncingBall.scala
+++ src/main/scala/com/gelisam/scage/base/BouncingBall.scala
@@ -1,5 +1,7 @@
 package com.gelisam.scage.base
 
+import com.gelisam.dylemma.frp.Behavior
+
 import com.github.dunnololda.scage.ScageLib._
 import com.github.dunnololda.scage.ScageScreenApp
 import com.github.dunnololda.scage.support.tracer3.{CoordTracer, DefaultTrace}
@@ -10,13 +12,45 @@ import scala.collection.mutable.ArrayBuffer
 
 object Settings {
   val worldSize = Vec(640, 480)
-  val pixelsPerSecond = 500.0
+  val pixelsPerSecond = 100.0
   val ballRadius = 10
 }
 
-object BouncingBall extends ScageScreenApp("Bounding Ball", Settings.worldSize.ix, Settings.worldSize.iy) {
-  var ballPos = Settings.worldSize / 2
-  var ballDir = Vec(1,1)
+import Settings._
+
+object BouncingBall
+  extends ScageScreenApp("Bounding Ball", worldSize.ix, worldSize.iy)
+  with Observer
+{
+  val tickE = EventSource[Long]  // in milliseconds
+  val timeB = Behavior.stepper(0.0, tickE.map(_ / 1000.0))  // in seconds
+  
+  lazy val timeAtStartB: Behavior[Double] = Behavior.stepper(0.0, bounceE.map(_ => timeB))
+  lazy val timeSinceStartB: Behavior[Double] = (timeB map2 timeAtStartB)(_-_)
+  
+  lazy val startPosB: Behavior[Vec] = Behavior.stepper(center, bounceE.map(_ => ballPosB))
+  lazy val ballPosB: Behavior[Vec] = Behavior.stepper(center, timeSinceStartB.changeE.map(t =>
+    startPosB.value + ballDirB.value * t * pixelsPerSecond
+  ))
+  
+  lazy val ballDxB = Behavior.stepper(1,
+    bounceRightE.map(_ => 1) ||
+    bounceLeftE.map(_ => -1)
+  )
+  lazy val ballDyB = Behavior.stepper(1,
+    bounceUpE.map(_ => 1) ||
+    bounceDownE.map(_ => -1)
+  )
+  lazy val ballDirB = (ballDxB map2 ballDyB)(Vec(_,_))
+  
+  lazy val bounceRightE = tickE.filter(_ => ballPosB.x - ballRadius < 0)
+  lazy val bounceUpE    = tickE.filter(_ => ballPosB.y - ballRadius < 0)
+  lazy val bounceLeftE  = tickE.filter(_ => ballPosB.x + ballRadius > worldSize.x)
+  lazy val bounceDownE  = tickE.filter(_ => ballPosB.y + ballRadius > worldSize.y)
+  lazy val bounceE = bounceRightE ||
+                     bounceUpE    ||
+                     bounceLeftE  ||
+                     bounceDownE
   
   keyIgnorePause(KEY_ESCAPE, onKeyDown = {
     stopApp()
@@ -25,29 +59,12 @@ object BouncingBall extends ScageScreenApp("Bounding Ball", Settings.worldSize.i
     switchPause()
   })
 
-  var lastTimestamp = 0L
-  init {
-    lastTimestamp = System.currentTimeMillis
-  }
   action {
-    val now = System.currentTimeMillis
-    val seconds = (now - lastTimestamp).toDouble / 1000
-    lastTimestamp = now
-    
-    val pixels = Settings.pixelsPerSecond * seconds
-    ballPos += pixels * ballDir
-    
-    if (ballPos.x - Settings.ballRadius < 0)
-      ballDir = ballDir.copy(x = 1)
-    if (ballPos.y - Settings.ballRadius < 0)
-      ballDir = ballDir.copy(y = 1)
-    if (ballPos.x + Settings.ballRadius > Settings.worldSize.x)
-      ballDir = ballDir.copy(x = -1)
-    if (ballPos.y + Settings.ballRadius > Settings.worldSize.y)
-      ballDir = ballDir.copy(y = -1)
+    tickE fire msecsFromInitWithoutPause
+    Behavior.updateBehaviors
   }
   render {
-    drawFilledCircle(ballPos, Settings.ballRadius, ScageColor.RED)
+    drawFilledCircle(ballPosB, ballRadius, ScageColor.RED)
     if (onPause) {
       print(s"PAUSED",        windowCenter, DARK_GRAY, align = "center")
     }
