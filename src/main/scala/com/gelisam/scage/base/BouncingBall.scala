eb20505f166d77e413fef4f6a89122f3c9fbe656
Samuel Gélineau <gelisam+github@gmail.com>
2015-08-11 22:59:58 -0400

    fix off by one frame issues

--- src/main/scala/com/gelisam/scage/base/BouncingBall.scala
+++ src/main/scala/com/gelisam/scage/base/BouncingBall.scala
@@ -22,32 +22,34 @@ object BouncingBall
   extends ScageScreenApp("Bouncing Ball", worldSize.ix, worldSize.iy)
   with Observer
 {
-  val tickE = EventSource[Long]  // in milliseconds
-  val timeB = Behavior.stepper(0.0, tickE.map(_ / 1000.0))  // in seconds
+  val tickE = EventSource[Long]      // in milliseconds
+  val timeE = tickE.map(_ / 1000.0)  // in seconds
+  val timeB = Behavior.stepper(0.0, timeE)
   
   lazy val timeAtStartB: Behavior[Double] = Behavior.stepper(0.0, bounceE.map(_ => timeB))
-  lazy val timeSinceStartB: Behavior[Double] = (timeB map2 timeAtStartB)(_-_)
+  lazy val timeSinceStartE: EventStream[Double] = timeE.map(_ - timeAtStartB)
   
-  lazy val startPosB: Behavior[Vec] = Behavior.stepper(center, bounceE.map(_ => ballPosB))
-  lazy val ballPosE: EventStream[Vec] = tickE.map(_ =>
-    startPosB.value + ballDirB.value * timeSinceStartB.value * pixelsPerSecond
+  lazy val startPosB: Behavior[Vec] = Behavior.stepper(center, bounceE.map(ball => ball))
+  lazy val ballPosE: EventStream[Vec] = timeSinceStartE.map(t =>
+    startPosB.value + ballDirB.value * t * pixelsPerSecond
   )
   lazy val ballPosB: Behavior[Vec] = Behavior.stepper(center, ballPosE)
   
-  lazy val ballDxB = Behavior.stepper(1,
+  lazy val ballDxB: Behavior[Int] = Behavior.stepper(1,
     bounceRightE.map(_ => 1) ||
     bounceLeftE.map(_ => -1)
   )
-  lazy val ballDyB = Behavior.stepper(1,
+  lazy val ballDyB: Behavior[Int] = Behavior.stepper(1,
     bounceUpE.map(_ => 1) ||
     bounceDownE.map(_ => -1)
   )
   lazy val ballDirB = (ballDxB map2 ballDyB)(Vec(_,_))
   
-  lazy val bounceRightE = ballPosE.filter(ball => ball.x - ballRadius < 0)
-  lazy val bounceUpE    = ballPosE.filter(ball => ball.y - ballRadius < 0)
-  lazy val bounceLeftE  = ballPosE.filter(ball => ball.x + ballRadius > worldSize.x)
-  lazy val bounceDownE  = ballPosE.filter(ball => ball.y + ballRadius > worldSize.y)
+  lazy val bounceRightE = ballPosE.filter(ball => ball.x - ballRadius < 0           && ballDxB.value == -1)
+  lazy val bounceUpE    = ballPosE.filter(ball => ball.y - ballRadius < 0           && ballDyB.value == -1)
+  lazy val bounceLeftE  = ballPosE.filter(ball => ball.x + ballRadius > worldSize.x && ballDxB.value ==  1)
+  lazy val bounceDownE  = ballPosE.filter(ball => ball.y + ballRadius > worldSize.y && ballDyB.value ==  1)
+  
   lazy val bounceE = bounceRightE ||
                      bounceUpE    ||
                      bounceLeftE  ||
