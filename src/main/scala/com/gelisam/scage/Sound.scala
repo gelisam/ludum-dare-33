c3c8cfad537c29a7dd478ae4efa1d48e95186b5b
Samuel Gélineau <gelisam+github@gmail.com>
2015-08-19 23:24:17 -0400

    fix: sounds were not playing in sbt mode

--- src/main/scala/com/gelisam/scage/Sound.scala
+++ src/main/scala/com/gelisam/scage/Sound.scala
@@ -6,13 +6,24 @@ import javax.sound.sampled._
 // I can't believe it! The API really requires us to read from the disk every time??
 case class Sound(private val file: File) {
   def play = {
-    val clip: Clip = AudioSystem.getClip
-    clip.open(AudioSystem.getAudioInputStream(file))
+    val clip = Sound.loadClip(file)
     clip.start
   }
 }
 
 object Sound {
+  // Need to mess with the classloader, see http://stackoverflow.com/a/25083123/3286185
+  def loadClip(file: File): Clip = {
+    val cl = classOf[javax.sound.sampled.AudioSystem].getClassLoader
+    val old = Thread.currentThread.getContextClassLoader
+    try {
+      Thread.currentThread.setContextClassLoader(cl)
+      val clip: Clip = AudioSystem.getClip
+      clip.open(AudioSystem.getAudioInputStream(file))
+      clip
+    } finally Thread.currentThread.setContextClassLoader(old)
+  }
+  
   def apply(filename: String): Sound =
     Sound(new File(filename))
 }
