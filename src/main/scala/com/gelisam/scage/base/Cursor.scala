7f23e80717c8c369266572ca5209dec2e604264c
Samuel Gélineau <gelisam+github@gmail.com>
2015-08-20 23:20:12 -0400

    press backtick to toggle the cursor

--- src/main/scala/com/gelisam/scage/base/Cursor.scala
+++ src/main/scala/com/gelisam/scage/base/Cursor.scala
@@ -10,14 +10,21 @@ import com.github.dunnololda.scage.handlers.controller2.ScageController
 import com.github.dunnololda.scage.ScageLib._
 import io.dylemma.frp._
 
+import java.awt.event.KeyEvent
+
 class Cursor(
   scage: ScageController with Renderer
 )(implicit observer: Observer) {
+  var enabled = false
+  def toggle =
+    enabled = !enabled
+  
   var cursorPos = scage.center
-  def moveCursor(newPos: Vec) {
+  def moveCursor(newPos: Vec) = if (enabled) {
     cursorPos = newPos
   }
   
+  scage.key(KEY_GRAVE, onKeyDown = toggle)
   scage.leftMouse(
     onBtnDown = moveCursor(_),
     onBtnUp = moveCursor(_)
@@ -26,7 +33,7 @@ class Cursor(
     onDrag = moveCursor(_)
   )
   
-  def render() {
+  def render() = if (enabled) {
     print(s"${cursorPos}", scage.windowCenter, DARK_GRAY, align = "center")
     
     drawLine(cursorPos - Vec(10, 0), cursorPos + Vec(10, 0), ScageColor.WHITE)
