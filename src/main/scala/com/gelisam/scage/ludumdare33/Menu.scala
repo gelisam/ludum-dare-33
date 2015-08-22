773a91f42dcecfae50e9cdb56194128cb309a43b
Samuel Gélineau <gelisam+github@gmail.com>
2015-08-22 16:16:19 -0400

    display menu items

--- src/main/scala/com/gelisam/scage/ludumdare33/Menu.scala
+++ src/main/scala/com/gelisam/scage/ludumdare33/Menu.scala
@@ -12,10 +12,19 @@ class Menu {
   val sprite = Sprite("menu.png")
   val pos = Adjustable[Vec]("menuPos")
   
+  val menuCmdPos = Adjustable[Vec]("menuCmdPos")
+  val menuCmdGap = Adjustable[Double]("menuCmdGap")
+  val cmdArray = Array[String]("Fight", "", "Magic", "Item")
+  
   def render {
     openglLocalTransform {
       openglMove(Vec(-234, -215) + pos)
       sprite.render()
+      
+      for (i <- 0 until cmdArray.length) {
+        val cmd = cmdArray(i)
+        print(cmd, Vec(-23, 90) + menuCmdPos + Vec(0, 3*menuCmdGap) * i, BLACK)
+      }
     }
   }
 }
