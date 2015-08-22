4a9a5940420d63a2014978bf292f83136cdebc48
Samuel Gélineau <gelisam+github@gmail.com>
2015-08-22 14:04:29 -0400

    use the new sprite system

--- src/main/scala/com/gelisam/scage/ludumdare33/Main.scala
+++ src/main/scala/com/gelisam/scage/ludumdare33/Main.scala
@@ -3,10 +3,9 @@ package com.gelisam.scage.ludumdare33
 import com.gelisam.scage.ludumdare33._
 import com.gelisam.scage.ludumdare33.Settings._
 
-import org.lwjgl.opengl.GL11
-
 import com.gelisam.scage.adjust._
 import com.gelisam.scage.adjust.Predef._
+import com.gelisam.scage.Sprite
 
 import com.gelisam.dylemma.frp.Behavior
 
@@ -17,11 +16,14 @@ object Main
   extends ScageScreenApp("Treasure Chest Life - Ludum Dare 33", worldSize.ix, worldSize.iy)
   with Observer
 {
+  init {
+    Adjust.init(this)
+    Sprite.init(this)
+  }
+  
   val tickE = EventSource[Long]      // in milliseconds
   val timeE = tickE.map(_ / 1000.0)  // in seconds
   val timeB = Behavior.stepper(0.0, timeE)
-  
-  Adjust.init(this)
   
   keyIgnorePause(KEY_ESCAPE, onKeyDown = {
     stopApp()
@@ -41,11 +43,10 @@ object Main
     //println(runtime.totalMemory() - runtime.freeMemory())
   }
   
-  val monsterImage = image("monster.png", 200, 175, 0, 0, 40, 35)
+  val monsterSprite = Sprite("monster.png", 5)
   val monsterPos = Adjustable[Vec]("monsterPos")
   render {
-    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
-    drawDisplayList(monsterImage, monsterPos)
+    monsterSprite.render(monsterPos)
     
     Adjust.render(this)
     
