56fbdae13fe4738415f205228391c9a616bdbd4d
Samuel Gélineau <gelisam+github@gmail.com>
2015-08-22 14:03:15 -0400

    better sprite system

---
+++ src/main/scala/com/gelisam/scage/Sprite.scala
@@ -0,0 +1,26 @@
+// Place your PNG files in the src/main/resources/resources/images folder.
+package com.gelisam.scage
+
+import com.github.dunnololda.scage.ScageLib._
+import org.lwjgl.opengl.GL11
+
+class Sprite(spriteId: Int) {
+  def render(offset: Vec = Vec(0, 0)) =
+    drawDisplayList(spriteId, offset)
+}
+
+object Sprite {
+  def init(scage: Object) = {
+    // make pixels crisp
+    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+  }
+  
+  def apply(filename: String, scale: Int = 1): Sprite = {
+    val fullFilename = s"resources/images/${filename}"
+    val texture = getTexture(fullFilename)
+		val width: Float = texture.getTextureWidth
+		val height: Float = texture.getTextureHeight
+    val spriteId = image(filename, width*scale, height*scale, 0, 0, width, height)
+    new Sprite(spriteId)
+  }
+}
